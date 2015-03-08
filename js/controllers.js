109fc5c82e1b856928435ec2e94be69a1b6a012a
shurik <stu.shurik@gmail.com>
2015-03-08 22:15:03 +0200

    add sound

--- js/controllers.js
+++ js/controllers.js
@@ -15,7 +15,25 @@ angular.module('ngPicture2sound.controllers', [])
 
                 $scope.animals = response.slice(0,6);
 
-            })
+            });
+
+
+            function change(sourceUrl) {
+
+
+
+                /****************/
+            }
+
+            $scope.say = function(soundSrc){
+                var audio = angular.element("#player");
+                angular.element("#sound_src").attr("src", soundSrc);
+                audio[0].pause();
+                audio[0].currentTime = 0;
+
+                audio[0].load();
+                audio[0].play();
+            }
 
         }
 
